4c205df694587a3a6c02f752796cfe281642bec0
Mangloo69 <yadav69.swayam@gmail.com>
2025-10-27 18:08:35 +0545

    Project setup, added lights rays in layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
 const Page = () => {
-  return <div>Welcome to NextJs 16</div>;
+  return (
+    <section>
+      <h1 className="text-center">
+        The Hub for Every Dev <br />
+        Event You Can&apos;t Miss
+      </h1>
+      <p className="text-center mt-5">
+        Hackathons, Meetups, and Conferences, All in One Place
+      </p>
+    </section>
+  );
 };
 
 export default Page;
